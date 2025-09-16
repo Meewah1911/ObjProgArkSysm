552031b98ce1fc94105dfa8d78fc4bbb8e8b0955
Michael Walle <michael.walle@pm.me>
2025-09-16 15:59:37 +0200

    Some small progress ig

--- Grunnleggende Objektorientert programmering/oppgaverarray.cpp
+++ Grunnleggende Objektorientert programmering/oppgaverarray.cpp
@@ -2,8 +2,39 @@
 using namespace std;
 
 int main(){
-    int arr[5] = {10,20,30,40,50};
-    cout << sizeof(arr) << endl;
-    cout << sizeof(arr[0]) << endl;
-    cout << sizeof(arr)/sizeof(arr[0]);
+    //Oppgave 1
+    cout << "Oppgave 1" << endl << endl;
+    int arr[5] = {10, 20, 30, 40, 50};
+
+    int* p_arr = arr;
+
+    for (size_t i = 0; i < size(arr); i++){
+        cout << p_arr[i] << endl;
+    } 
+
+    //Oppgave 2
+    cout << endl << "Oppgave 2" << endl << endl;
+    int *p_left = nullptr;
+    int *p_right = nullptr;
+    int temp;
+
+    p_left = arr;
+    temp = *p_left;
+    p_right = &arr[size(arr) - 1];
+
+    while(p_left < p_right){
+        p_left = arr;
+        temp = *p_left;
+        p_right = &arr[size(arr) - 1];
+        
+        cout << *p_left << "  " << *p_right << endl;
+
+        *p_left = *p_right;
+        *p_right = temp;
+
+        cout << *p_left << "  " << *p_right;
+
+        ++p_left;
+        --p_right;
+    }
 }
