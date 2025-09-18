b39fa51d3a3b8b5ef652d856536976c88857a719
Michael Walle <michael.walle@pm.me>
2025-09-18 10:30:13 +0200

    Oppgave 2

--- Grunnleggende Objektorientert programmering/Funksjoner.cpp
+++ Grunnleggende Objektorientert programmering/Funksjoner.cpp
@@ -2,16 +2,23 @@
 using namespace std;
 
 //Oppgave 1
-void square(int *x){
+int square(int *x){
     if (x != nullptr){
-        *x = (*x) * (*x);
+        return *x = (*x) * (*x);
+    }
+}
+
+//Oppgave 2
+bool isEven(int *x){
+    if(x != nullptr){
+        return *x % 2 == 0;
     }
 }
 
 int main(){
     int a_variabel = 5;
+    int b_variabel = 10;
 
     square(&a_variabel);
-    cout << a_variabel;
-
+    cout << a_variabel << endl;   
 }
